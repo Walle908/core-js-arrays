e1fb8e6be8e2fb5f3c358b05b87b345ca51cec5f
Walle908 <walle0908@yandex.ru>
2025-11-15 01:00:26 +0400

    feat: add solution for calculateBalance, createChunks, generateOdds, getFalsyValuesCount

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -315,8 +315,8 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr[0][0] - arr[0][1] + arr[1][0] - arr[1][1];
 }
 
 /**
@@ -331,8 +331,14 @@ function calculateBalance(/* arr */) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return arr.reduce(
+    (chunks, item, ind) =>
+      ind % chunkSize === 0
+        ? [...chunks, arr.slice(ind, ind + chunkSize)]
+        : chunks,
+    []
+  );
 }
 
 /**
@@ -347,8 +353,8 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array.from({ length: len }, (item, ind) => ind + (ind + 1));
 }
 
 /**
@@ -379,8 +385,8 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.filter((item) => !item).length;
 }
 
 /**
